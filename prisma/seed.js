7de9226c29c5256a93c8bdbf61beaf94e65fd687
Brandon Hamilton <bham2015@gmail.com>
2024-10-14 11:51:41 -0500

    Completed the Guided Practice.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,25 @@
 const prisma = require("../prisma");
-const seed = async () => {
+const seed = async (numRestaurants = 3, numReservations = 5) => {
   // TODO: Create 3 restaurants with 5 reservations each
+  for (let i = 0; i < numRestaurants; i++) {
+    const reservations = [];
+    for (let j = 0; j < numReservations; j++) {
+      reservations.push({
+        name: `Person ${i}${j}`,
+        email: `${i}${j}@foo.bar`,
+        partySize: Math.floor(Math.random() * 10) + 1,
+      });
+    }
+
+    await prisma.restaurant.create({
+      data: {
+        name: `Restaurant ${i + 1}`,
+        reservations: {
+          create: reservations,
+        },
+      },
+    });
+  }
 };
 seed()
   .then(async () => await prisma.$disconnect())
